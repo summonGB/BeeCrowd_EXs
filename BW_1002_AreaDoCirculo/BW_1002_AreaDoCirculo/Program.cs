03d80442ccfb8cde06399ac6e13168d73d34b80f
Gabriel Barbosa <gabrielemail060@gmail.com>
2024-07-26 13:31:14 -0300

    1002

---
+++ BW_1002_AreaDoCirculo/BW_1002_AreaDoCirculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double raio, pi, raio1, area;
+
+        pi = 3.14159;
+
+        raio1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        raio = Math.Pow(raio1, 2.0);
+
+        area = pi * raio;
+
+
+        Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+    }
+}
