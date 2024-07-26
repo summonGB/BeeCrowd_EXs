5d0698eaf2550b1c8c9924e6b84f288c9201d581
Gabriel Barbosa <gabrielemail060@gmail.com>
2024-07-26 12:06:04 -0300

    1001

---
+++ BW_1001_ExtremamenteBasico/BW_1001_ExtremamenteBasico/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int A, B, X;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+
+        X = A + B;
+
+        Console.WriteLine("X = " + X);
+
+    }
+}
