f70473907e895d65365ce4f3b32ac6dff7146013
Gabriel Barbosa <gabrielemail060@gmail.com>
2024-07-27 00:26:25 -0300

    1003

---
+++ BW_1003_SomaSimples/BW_1003_SomaSimples/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int A, B, SOMA;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+
+        SOMA = A + B;
+
+        Console.WriteLine("SOMA = " + SOMA);
+
+    }
+}
