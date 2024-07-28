6d190b258dfaba0b41b9dc0777126a94ef297861
Gabriel Barbosa <gabrielemail060@gmail.com>
2024-07-28 13:52:01 -0300

    1010,1014,1016,1017

---
+++ BW_1017_ConsumoDeCombustivel/BW_1017_ConsumoDeCombustivel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double TempV, VelV, DistV, Consu;
+
+        TempV = int.Parse(Console.ReadLine());
+        VelV = int.Parse(Console.ReadLine());
+
+        DistV =  VelV * TempV;
+
+        //Console.WriteLine(DistV);
+        Consu = DistV / 12;
+
+        Console.WriteLine(Consu.ToString("F5"));
+    }
+}
